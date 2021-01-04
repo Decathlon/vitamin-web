d02cd37ac64fdb780a591d64c6b51f8f12a71f45
Laurent Thiebault <thiebault.laurent@gmail.com>
2021-01-05 00:08:24 +0100

    refactor: add color-mod function to manage alpha modifications

--- packages/css/.storybook/main.js
+++ packages/css/.storybook/main.js
@@ -4,9 +4,5 @@ module.exports = {
     '../src/stories/guidelines/**/*.stories.(mdx|js)',
     '../src/stories/components/**/*.stories.(mdx|js)',
   ],
-  addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-storysource',
-  ],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
 };
