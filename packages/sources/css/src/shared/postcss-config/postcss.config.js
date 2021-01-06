a58fc39f29df6bbe1eef100e2a60108a51bcccf7
Laurent Thiebault <thiebault.laurent@gmail.com>
2021-01-07 00:20:50 +0100

    wip laurent 2

--- packages/sources/css/src/shared/postcss-config/postcss.config.js
+++ packages/sources/css/src/shared/postcss-config/postcss.config.js
@@ -1,9 +1,11 @@
 // const postcss = require('postcss');
 const postcssImport = require('postcss-import');
 const postcssColorMod = require('postcss-color-mod-function');
+const postcssCombineDuplicatedSelectors = require('postcss-combine-duplicated-selectors');
 const autoprefixer = require('autoprefixer');
 const tailwindcss = require('tailwindcss');
 const postcssCssVariables = require('postcss-css-variables');
+const postcssDiscardComments = require('postcss-discard-comments');
 
 module.exports = {
   plugins: [
@@ -12,5 +14,7 @@ module.exports = {
     tailwindcss,
     autoprefixer,
     postcssCssVariables,
+    postcssCombineDuplicatedSelectors,
+    postcssDiscardComments,
   ],
 };
