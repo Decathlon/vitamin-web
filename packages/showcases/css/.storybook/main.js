4161825135ba330cc2422e3aa28731c3e88db273
Laurent Thiebault <thiebault.laurent@gmail.com>
2021-01-09 01:49:00 +0100

    refactor(showcase-css): add addon-design-token

--- packages/showcases/css/.storybook/main.js
+++ packages/showcases/css/.storybook/main.js
@@ -14,5 +14,6 @@ module.exports = {
     'storybook-addon-designs',
     'storybook-mobile',
     'storybook-addon-outline',
+    'storybook-design-token',
   ],
 };
--- packages/showcases/css/.storybook/preview.js
+++ packages/showcases/css/.storybook/preview.js
@@ -3,6 +3,15 @@ import { withDesign } from 'storybook-addon-designs';
 import viewports from './addons/viewports.json';
 import backgrounds from './addons/backgrounds.json';
 
+const cssReq = require.context(
+  '!!raw-loader!@decathlon/vitamin-css-design-tokens/src',
+  true,
+  /.\.css$/,
+);
+const cssTokenFiles = cssReq
+  .keys()
+  .map((filename) => ({ filename, content: cssReq(filename).default }));
+
 export const decorators = [withDesign];
 
 export const parameters = {
@@ -10,4 +19,12 @@ export const parameters = {
     viewports,
   },
   backgrounds,
+  designToken: {
+    files: {
+      css: cssTokenFiles,
+    },
+    options: {
+      hideMatchingHardCodedValues: false,
+    },
+  },
 };
