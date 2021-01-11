32b5691b3d712e28d8b9e5e19899d46ae1abd8ac
Laurent Thiebault <thiebault.laurent@gmail.com>
2021-01-11 15:49:05 +0100

    refactor: postcss typo

--- packages/sources/css/postcss.config.js
+++ packages/sources/css/postcss.config.js
@@ -1,8 +1,8 @@
-const postCssImport = require('postcss-import');
+const postcssImport = require('postcss-import');
 const postcssColorMod = require('postcss-color-mod-function');
 const tailwindcss = require('tailwindcss');
 const autoprefixer = require('autoprefixer');
 
 module.exports = {
-  plugins: [postCssImport, postcssColorMod, tailwindcss, autoprefixer],
+  plugins: [postcssImport, postcssColorMod, tailwindcss, autoprefixer],
 };
