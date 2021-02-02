80a6fe37da0f78a3fa21294c2a0e8658f777b6f2
Laurent Thiebault <thiebault.laurent@gmail.com>
2021-02-02 18:36:35 +0100

    refactor: tailwindcss & postcss configurations

--- packages/sources/css/config/postcss/postcss.config.js
+++ packages/sources/css/config/postcss/postcss.config.js
@@ -2,7 +2,14 @@ const postcssImport = require('postcss-import');
 const postcssColorMod = require('postcss-color-mod-function');
 const tailwindcss = require('tailwindcss');
 const autoprefixer = require('autoprefixer');
+const postcssCombineDuplicatedSelectors = require('postcss-combine-duplicated-selectors');
 
 module.exports = {
-  plugins: [postcssImport, postcssColorMod, tailwindcss, autoprefixer],
+  plugins: [
+    postcssImport,
+    postcssColorMod,
+    tailwindcss,
+    autoprefixer,
+    postcssCombineDuplicatedSelectors,
+  ],
 };
