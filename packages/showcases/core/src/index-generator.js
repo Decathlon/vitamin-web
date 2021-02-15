be36f52c73190c8b84c0505c4f7a7e363f2dfbd5
Laurent Thiebault <thiebault.laurent@gmail.com>
2021-02-15 01:49:58 +0100

    chore: fix storybook-deployer

--- packages/showcases/core/src/index-generator.js
+++ packages/showcases/core/src/index-generator.js
@@ -2,18 +2,18 @@ const fs = require('fs');
 const path = require('path');
 
 const colors = [
-  'brand-digital-light-1',
   'brand-digital',
   'brand-digital-dark-1',
   'brand-digital-dark-2',
 ];
 
 const generateRow = (package, index) => `
-  <a href="${package.name}">
-    <span class="vtmn-text-${colors[index % colors.length]}">
-      ${package.name}
-    </span>
-    <span class="description">${package.description}</span>
+  <a href="${
+    package.name
+  }" target="_blank" class="vtmn-flex vtmn-justify-center vtmn-bg-${
+  colors[index % colors.length]
+} vtmn-text-white hover:vtmn-bg-brand-digital-light-1 vtmn-rounded-lg vtmn-p-5 vtmn-mb-4" style="width: 400px">
+    ${package.name}
   </a>
 `;
 
@@ -33,35 +33,16 @@ const generateHTML = (packages) => `
       href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:ital,wght@0,400;0,700;1,700&display=swap"
       rel="stylesheet"
     />
-    <link rel="stylesheet" type="text/css" href="http://unpkg.com/@vtmn/css">
+    <link rel="stylesheet" type="text/css" href="https://unpkg.com/@vtmn/css">
   </head>
   <body>
-    <img class="vtmn-text-center" src="https://user-images.githubusercontent.com/9600228/102414461-e3b92b00-3ff6-11eb-9c96-5f37c4d5e02c.png" alt="Decathlon Design System - Vitamin"/>
-    <div class="content">
+    <div class="vtmn-flex vtmn-flex-col vtmn-justify-center vtmn-items-center">
+      <img width="400" class="vtmn-m-8" src="https://user-images.githubusercontent.com/9600228/102414461-e3b92b00-3ff6-11eb-9c96-5f37c4d5e02c.png" alt="Decathlon Design System - Vitamin"/>
       ${packages.map(generateRow).join('')}
-    </div>
   </body>
   </html>
 `;
 
-module.exports = function buildMonorepoIndex(
-  packages,
-  customHTMLGenerate,
-  outputDir,
-) {
-  let index;
-
-  console.log('=> Building index.html for monorepo');
-
-  if (customHTMLGenerate) {
-    const fn = require(path.join(process.cwd(), customHTMLGenerate));
-
-    if (typeof fn === 'function') {
-      index = fn(packages, outputDir);
-    }
-  } else {
-    index = generateHTML(packages);
-  }
-
-  fs.writeFileSync(path.join(outputDir, 'index.html'), index);
+module.exports = function buildMonorepoIndex(packages, outputDir) {
+  return generateHTML(packages);
 };
