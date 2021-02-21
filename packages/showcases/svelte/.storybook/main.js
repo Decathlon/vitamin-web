ad820a19dc1743c97f1c03e61dfc2a5ab43b7fef
Laurent Thiebault <laurent.thiebault@decathlon.com>
2021-02-21 01:06:05 +0100

    docs(@vtmn/svelte): enhance documentation + showcase (#124)

--- packages/showcases/svelte/.storybook/main.js
+++ packages/showcases/svelte/.storybook/main.js
@@ -1,10 +1,14 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  'stories': [
+    '../stories/**/*.stories.mdx',
+    '../stories/**/*.stories.@(js|jsx|ts|tsx)'
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials"
+  'addons': [
+    '@storybook/addon-essentials',
+    '@storybook/addon-a11y',
+    '@whitespace/storybook-addon-html',
+    'storybook-addon-designs',
+    'storybook-mobile',
+    'storybook-addon-outline',
   ]
 }
--- packages/showcases/svelte/.storybook/preview.js
+++ packages/showcases/svelte/.storybook/preview.js
@@ -1,3 +1,14 @@
+
+import { withDesign } from 'storybook-addon-designs';
+import backgrounds from '@vtmn/showcase-core/addons/backgrounds.json';
+import viewports from '@vtmn/showcase-core/addons/viewports.json';
+
+export const decorators = [withDesign];
+
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-}
+  actions: { argTypesRegex: '^on[A-Z].*' },
+  backgrounds,
+  viewport: {
+    viewports,
+  },
+};
