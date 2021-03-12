af50104d9891f23b0493c35b3a2bc91c9dac698d
Laurent Thiebault <thiebault.laurent@gmail.com>
2021-03-12 01:41:24 +0100

    feat(@vtmn/css-design-tokens): generate .scss output

--- packages/sources/css/config/postcss/postcss-core.config.js
+++ packages/sources/css/config/postcss/postcss-core.config.js
@@ -5,18 +5,28 @@ const postcssCombineDuplicatedSelectors = require('postcss-combine-duplicated-se
 const postcssCombineMediaQuery = require('postcss-combine-duplicated-selectors');
 const postcssDiscardComments = require('postcss-discard-comments');
 const postcssRem = require('postcss-rem');
+const postcssCssVarToSassVar = require('postcss-css-var-to-sass-var');
+const postcssPrettify = require('postcss-prettify');
 
 const postcssRemOptions =
   process.env.FONT_SIZE_BASE === '10' ? { baseline: 10 } : { baseline: 16 };
 
+let plugins = [
+  postcssImport,
+  postcssColorMod,
+  autoprefixer,
+  postcssDiscardComments,
+  postcssCombineMediaQuery,
+  postcssCombineDuplicatedSelectors,
+  postcssRem(postcssRemOptions),
+];
+
+if (process.env.SCSS_FORMAT === 'true') {
+  plugins = [...plugins, postcssCssVarToSassVar];
+}
+
+plugins = [...plugins, postcssPrettify];
+
 module.exports = {
-  plugins: [
-    postcssImport,
-    postcssColorMod,
-    autoprefixer,
-    postcssDiscardComments,
-    postcssCombineMediaQuery,
-    postcssCombineDuplicatedSelectors,
-    postcssRem(postcssRemOptions),
-  ],
+  plugins,
 };
