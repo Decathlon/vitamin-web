fc4e9f6a4719688b70ae8b49eff2df5448e6d69e
Laurent Thiebault <thiebault.laurent@gmail.com>
2021-03-20 23:01:11 +0100

    refactor(@vtmn/icons): fix build of SVGs

--- packages/sources/icons/src/scripts/build-svg.js
+++ packages/sources/icons/src/scripts/build-svg.js
@@ -1,8 +1,8 @@
 const fs = require('fs');
 const fse = require('fs-extra');
 
-const vitamixSrcDir = `${__dirname}/vitamix`;
-const vitamixDestDir = `${__dirname}/../dist/vitamix`;
+const vitamixSrcDir = `${__dirname}/../vitamix`;
+const vitamixDestDir = `${__dirname}/../../dist/vitamix`;
 
 fse.copy(vitamixSrcDir, `${vitamixDestDir}/svg`, { overwrite: true }, (err) => {
   if (err) {
