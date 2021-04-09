c4a9de1e3a3ad65e0b8b7cefda1030f57fe994a2
gabin.bloquet <gabin.bloquet@decathlon.com>
2021-04-09 16:27:15 +0200

    refactor: delete useless types

--- packages/sources/react/src/components/VtmnTextInput/types.ts
+++
@@ -1,9 +0,0 @@
-export type VtmnButtonVariant =
-  | 'primary'
-  | 'primary-reversed'
-  | 'secondary'
-  | 'secondary-reversed'
-  | 'ghost'
-  | 'conversion';
-
-export type VtmnButtonSize = 'small' | 'medium' | 'large' | 'stretched';
