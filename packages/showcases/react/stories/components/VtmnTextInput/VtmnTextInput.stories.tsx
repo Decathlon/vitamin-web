58d034a570841310bf85cf197862411c8f4e6074
Gabin Bloquet <gabin.bloquet@decathlon.com>
2021-04-16 07:51:51 +0200

    Update packages/showcases/react/stories/components/VtmnTextInput/VtmnTextInput.stories.tsx
    Co-authored-by: Laurent Thiebault <thiebault.laurent@gmail.com>

--- packages/showcases/react/stories/components/VtmnTextInput/VtmnTextInput.stories.tsx
+++ packages/showcases/react/stories/components/VtmnTextInput/VtmnTextInput.stories.tsx
@@ -12,12 +12,6 @@ export default {
       defaultValue: 'vtmn-input',
       control: { type: 'text' },
     },
-    className: {
-      type: { name: 'string', required: true },
-      description: 'The id of the input.',
-      defaultValue: 'vtmn-input',
-      control: { type: 'text' },
-    },
     label: {
       type: { name: 'string', required: true },
       description: 'The label of the input.',
