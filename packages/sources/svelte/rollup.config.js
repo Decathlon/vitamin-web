7966d898b9d0840181f7f92261df499bc220335b
thollander <terence.hollander@decathlon.com>
2021-04-24 21:50:36 +0200

    feat(vtmn-link): create svelte component

--- packages/sources/svelte/rollup.config.js
+++ packages/sources/svelte/rollup.config.js
@@ -4,7 +4,7 @@ import css from 'rollup-plugin-css-only';
 import sveltePreprocess from 'svelte-preprocess';
 const preprocessOptions = require('./svelte.config').preprocessOptions;
 
-const components = ['VtmnButton', 'VtmnTextInput'];
+const components = ['VtmnButton', 'VtmnTextInput', 'VtmnLink'];
 
 export default components.map((name) => ({
   input: `src/components/${name}.svelte`,
