d22c47d98729302ec5d37e05b74c2826df72fdb5
Gaspard Mathon <gaspard.mathon@hotmail.fr>
2021-05-11 10:33:20 +0200

    feat(@vtmn/css-badge): init badge component, repo, package and story

---
+++ packages/showcases/css/stories/components/badge/badge.stories.js
@@ -0,0 +1,55 @@
+import overview from './examples/overview.html';
+import small from './examples/small.html';
+import medium from './examples/medium.html';
+import large from './examples/large.html';
+
+export default {
+  title: 'Components / Badge ',
+  argTypes: { onchange: { action: 'write' } },
+  parameters: {
+    actions: {
+      handles: ['mouseenter', 'click', 'focusin', 'focusout'],
+    },
+    design: {
+      type: 'figma',
+      url:
+        'https://www.figma.com/file/zDZIyayUlr1yTWrsi7cFoo/Vtmn---Web?node-id=1450%3A8683',
+    },
+  },
+};
+
+export const Overview = () => overview;
+Overview.parameters = {
+  docs: {
+    source: {
+      code: overview,
+    },
+  },
+};
+
+export const Small = () => small;
+Small.parameters = {
+  docs: {
+    source: {
+      code: small,
+    },
+  },
+};
+
+export const Medium = () => medium;
+Medium.parameters = {
+  docs: {
+    source: {
+      code: medium,
+    },
+  },
+};
+
+export const Large = () => large;
+Large.parameters = {
+  docs: {
+    source: {
+      code: large,
+    },
+  },
+};
