f91ceea5af3f01d682a60a620cfa510394789e7f
Gaspard Mathon <gaspard.mathon@hotmail.fr>
2021-06-02 09:56:34 +0200

    Revert "Delete button.stories.js"
    This reverts commit 235dddb7eb5c62d2c49593d031a58cccdaecea73.

---
+++ packages/showcases/css/stories/components/button/button.stories.js
@@ -0,0 +1,97 @@
+import overview from './examples/overview.html';
+import primary from './examples/primary.html';
+import primaryReversed from './examples/primary-reversed.html';
+import secondary from './examples/secondary.html';
+import secondaryReversed from './examples/secondary-reversed.html';
+import ghost from './examples/ghost.html';
+import conversion from './examples/conversion.html';
+import withIcon from './examples/with-icon.html';
+
+export default {
+  title: 'Components / Button',
+  argTypes: { onclick: { action: 'clicked' } },
+  parameters: {
+    actions: {
+      handles: ['mouseenter', 'click', 'focusin', 'focusout'],
+    },
+    design: {
+      type: 'figma',
+      url:
+        'https://www.figma.com/file/zDZIyayUlr1yTWrsi7cFoo/Vtmn-Web?node-id=31%3A136',
+    },
+  },
+};
+
+export const Overview = () => overview;
+Overview.parameters = {
+  docs: {
+    source: {
+      code: overview,
+    },
+  },
+};
+
+export const WithIcon = () => withIcon;
+WithIcon.parameters = {
+  docs: {
+    source: {
+      code: withIcon,
+    },
+  },
+};
+
+export const Primary = () => primary;
+Primary.parameters = {
+  docs: {
+    source: {
+      code: primary,
+    },
+  },
+};
+
+export const PrimaryReversed = () => primaryReversed;
+PrimaryReversed.parameters = {
+  docs: {
+    source: {
+      code: primaryReversed,
+    },
+  },
+  backgrounds: { default: 'blue' },
+};
+
+export const Secondary = () => secondary;
+Secondary.parameters = {
+  docs: {
+    source: {
+      code: secondary,
+    },
+  },
+};
+
+export const SecondaryReversed = () => secondaryReversed;
+SecondaryReversed.parameters = {
+  docs: {
+    source: {
+      code: secondaryReversed,
+    },
+  },
+  backgrounds: { default: 'blue' },
+};
+
+export const Ghost = () => ghost;
+Ghost.parameters = {
+  docs: {
+    source: {
+      code: ghost,
+    },
+  },
+};
+
+export const Conversion = () => conversion;
+Conversion.parameters = {
+  docs: {
+    source: {
+      code: conversion,
+    },
+  },
+};
