29def4d256f738c29598b532c928911311c8fa49
Laurent Thiebault <thiebault.laurent@gmail.com>
2021-06-15 01:50:14 +0200

    feat(@vtmn/vue): add link component

---
+++ packages/sources/vue/src/components/VtmnLink/VtmnLink.vue
@@ -0,0 +1,41 @@
+<script lang="ts">
+import '@vtmn/css-link/dist/index.css';
+import { reactive, computed, defineComponent } from 'vue';
+
+export default /*#__PURE__*/ defineComponent({
+  name: 'VtmnLink',
+  props: {
+    size: {
+      type: String,
+      default: 'medium',
+      validator: (val: string) => ['small', 'medium', 'large'].includes(val),
+    },
+    isStandalone: {
+      type: Boolean,
+      default: false,
+    },
+    hasIconAlong: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  setup(props) {
+    props = reactive(props);
+
+    return {
+      classes: computed(() => ({
+        'vtmn-link': true,
+        [`vtmn-link_size--${props.size}`]: true,
+        'vtmn-link--standalone': props.isStandalone,
+        'vtmn-link--icon-along': props.hasIconAlong,
+      })),
+    };
+  },
+});
+</script>
+
+<template>
+  <a :class="classes" v-bind="$attrs">
+    <slot></slot>
+  </a>
+</template>
