ad9c7ed0343b869f9420b9ac07ca73f4933c9b76
Gaspard Mathon <gaspard.mathon@hotmail.fr>
2021-06-24 18:32:32 +0200

    feat(@vtmn/react): add checkbox component (#384)

--- packages/sources/react/src/index.ts
+++ packages/sources/react/src/index.ts
@@ -5,3 +5,4 @@ export { VtmnIcon } from './components/VtmnIcon';
 export { VtmnLink } from './components/VtmnLink';
 export { VtmnTextInput } from './components/VtmnTextInput';
 export { VtmnToggle } from './components/VtmnToggle';
+export { VtmnCheckbox } from './components/VtmnCheckbox';
