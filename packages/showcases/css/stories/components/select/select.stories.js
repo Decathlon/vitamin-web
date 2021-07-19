cc318c78d424bcd834ec4361d22b0aa170b08281
Gaspard Mathon <gaspard.mathon@hotmail.fr>
2021-07-19 12:15:30 +0200

    feat(@vtmn/css): add select component folder

---
+++ packages/showcases/css/stories/components/select/select.stories.js
@@ -0,0 +1,65 @@
+import overview from './examples/overview.html';
+/*import valid from './examples/valid.html';
+import error from './examples/error.html';
+import disabled from './examples/disabled.html';
+import multiline from './examples/multiline.html';*/
+
+export default {
+  title: 'Components / Select',
+  argTypes: { onchange: { action: 'write' } },
+  parameters: {
+    actions: {
+      handles: ['mouseenter', 'click', 'focusin', 'focusout'],
+    },
+    design: {
+      type: 'figma',
+      url: 'https://www.figma.com/file/zDZIyayUlr1yTWrsi7cFoo/Vitamin---Web-UI-kit-(v0)?node-id=2535%3A52',
+    },
+  },
+};
+
+export const Overview = () => overview;
+Overview.parameters = {
+  docs: {
+    source: {
+      code: overview,
+    },
+  },
+};
+/*
+export const Valid = () => valid;
+Valid.parameters = {
+  docs: {
+    source: {
+      code: valid,
+    },
+  },
+};
+
+export const Error = () => error;
+Error.parameters = {
+  docs: {
+    source: {
+      code: error,
+    },
+  },
+};
+
+export const Disabled = () => disabled;
+Disabled.parameters = {
+  docs: {
+    source: {
+      code: disabled,
+    },
+  },
+};
+
+export const Multiline = () => multiline;
+Multiline.parameters = {
+  docs: {
+    source: {
+      code: multiline,
+    },
+  },
+};
+*/
