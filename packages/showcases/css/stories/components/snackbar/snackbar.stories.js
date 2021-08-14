10f6d842ae9cf1f76801c0771a770b2b82480efe
Térence Hollander <terence.hollander@decathlon.com>
2021-08-14 21:00:33 +0200

    feat: nx as monorepo manager

--- packages/showcases/css/stories/components/snackbar/snackbar.stories.js
+++ packages/showcases/css/stories/components/snackbar/snackbar.stories.js
@@ -12,8 +12,7 @@ export default {
     },
     design: {
       type: 'figma',
-      url:
-        'https://www.figma.com/file/zDZIyayUlr1yTWrsi7cFoo/Vitamin---Web-UI-kit-(v0)?node-id=2796%3A12600',
+      url: 'https://www.figma.com/file/zDZIyayUlr1yTWrsi7cFoo/Vitamin---Web-UI-kit-(v0)?node-id=2796%3A12600',
     },
   },
 };
