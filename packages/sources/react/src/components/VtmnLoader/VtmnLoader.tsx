8099ad62b604a39bd205336b47a03e8e0e397a50
Gabin Bloquet <gabin.bloquet@decathlon.com>
2021-09-23 00:55:12 +0200

    feat(react): add loader component (#546)
    Co-authored-by: Laurent Thiebault <laurent.thiebault@decathlon.com>

---
+++ packages/sources/react/src/components/VtmnLoader/VtmnLoader.tsx
@@ -0,0 +1,26 @@
+import * as React from 'react';
+
+import '@vtmn/css-loader/dist/index-with-vars.css';
+import { VtmnLoaderSize } from './types';
+
+export interface VtmnLoaderProps extends React.ComponentPropsWithoutRef<'div'> {
+  /**
+   * The size of the loader.
+   * @default 'medium'
+   */
+  size?: VtmnLoaderSize;
+}
+
+export const VtmnLoader = ({
+  size = 'medium',
+  className,
+  ...props
+}: VtmnLoaderProps) => {
+  return (
+    <div
+      className={`vtmn-loader vtmn-loader_size--${size} ${className}`}
+      {...props}></div>
+  );
+};
+
+export default React.memo(VtmnLoader);
