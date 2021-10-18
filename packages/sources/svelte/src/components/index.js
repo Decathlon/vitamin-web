9e3bc5ed90fa9886d52704d8f26449fac468dfda
Anthony Le Goas <anthony.legoas.partner@decathlon.com>
2021-10-18 21:32:13 +0200

    fix(@vtmn/svelte): add @vtmn/css-* as dependencies (#689)
    * refactor(@vtmn/svelte): export all components from index
    
    * fix(@vtmn/svelte): add @vtmn/css-* as dependencies
    
    * fix(@vtmn/svelte): add svelte as peerDependency
    
    * Update packages/sources/svelte/package.json
    
    Co-authored-by: Laurent Thiebault <thiebault.laurent@gmail.com>

---
+++ packages/sources/svelte/src/components/index.js
@@ -0,0 +1,4 @@
+export { default as VtmnButton } from './VtmnButton.svelte';
+export { default as VtmnLink } from './VtmnLink.svelte';
+export { default as VtmnPopover } from './VtmnPopover.svelte';
+export { default as VtmnTextInput } from './VtmnTextInput.svelte';
