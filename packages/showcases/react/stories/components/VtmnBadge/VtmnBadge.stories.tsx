ca8c35e47ba7eb973f6ff5cf38ad33dd6e4ef70a
Laurent Thiebault <thiebault.laurent@gmail.com>
2021-10-27 13:53:12 +0200

    refactor(@vtmn/react): add export for types

--- packages/showcases/react/stories/components/VtmnBadge/VtmnBadge.stories.tsx
+++ packages/showcases/react/stories/components/VtmnBadge/VtmnBadge.stories.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { VtmnBadge } from '@vtmn/react';
 import { Meta, Story } from '@storybook/react';
-import { parameters } from '@vtmn/showcase-core/csf/components/VtmnBadge.csf';
+import {
+  argTypes,
+  parameters,
+} from '@vtmn/showcase-core/csf/components/VtmnBadge.csf';
 
 export default {
   title: 'Components/VtmnBadge',
   component: VtmnBadge,
+  argTypes,
   parameters,
 } as Meta;
 
