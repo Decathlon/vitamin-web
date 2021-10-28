cae555680fa03ea431e628f17daec458b2e29981
Gaspard MATHON <gaspard.mathon@hotmail.fr>
2021-10-28 14:58:38 +0200

    feat(@vtmn/css): add 3 card variants + showcase

--- packages/showcases/css/stories/components/card/card.stories.js
+++ packages/showcases/css/stories/components/card/card.stories.js
@@ -1,13 +1,42 @@
 import overview from './examples/overview.html';
+import { default as top } from './examples/top-image.html';
+import { default as side } from './examples/side-image.html';
+import { default as full } from './examples/full-image.html';
 
 export default {
   title: 'Components / Card ',
   argTypes: { onchange: { action: 'write' } },
-  parameters,
 };
 
 export const Overview = () => overview;
 Overview.parameters = {
+  docs: {
+    source: {
+      code: overview,
+    },
+  },
+};
+
+export const Top = () => top;
+Top.parameters = {
+  docs: {
+    source: {
+      code: top,
+    },
+  },
+};
+
+export const Side = () => side;
+Side.parameters = {
+  docs: {
+    source: {
+      code: side,
+    },
+  },
+};
+
+export const Full = () => full;
+Full.parameters = {
   docs: {
     source: {
       code: overview,
