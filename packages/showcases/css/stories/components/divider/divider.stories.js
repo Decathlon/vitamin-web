f91fa03fec1ac289dc786c6f27415fea10b24f66
Gaspard MATHON <gaspard.mathon@hotmail.fr>
2021-12-20 15:02:46 +0100

    feat(@vtmn/css): add two new showcases

--- packages/showcases/css/stories/components/divider/divider.stories.js
+++ packages/showcases/css/stories/components/divider/divider.stories.js
@@ -1,4 +1,6 @@
 import overview from './examples/overview.html';
+import horizontal from './examples/horizontal.html';
+import vertical from './examples/vertical.html';
 
 export default {
   title: 'Components / Divider',
@@ -10,6 +12,24 @@ Overview.parameters = {
   docs: {
     source: {
       code: overview,
+    },
+  },
+};
+
+export const Horizontal = () => horizontal;
+Horizontal.parameters = {
+  docs: {
+    source: {
+      code: horizontal,
+    },
+  },
+};
+
+export const Vertical = () => vertical;
+Vertical.parameters = {
+  docs: {
+    source: {
+      code: vertical,
     },
   },
 };
