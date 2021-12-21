c64dcf516fed1209d2c1cbe889075855af29c826
Gaspard MATHON <gaspard.mathon@hotmail.fr>
2021-12-21 16:00:07 +0100

    feat(@vtmn/css): add showcases

--- packages/showcases/css/stories/components/search/search.stories.js
+++ packages/showcases/css/stories/components/search/search.stories.js
@@ -1,4 +1,7 @@
 import overview from './examples/overview.html';
+import _default from './examples/default.html';
+import persistent from './examples/persistent.html';
+import onContent from './examples/on-content.html';
 
 export default {
   title: 'Components / Search',
@@ -10,6 +13,33 @@ Overview.parameters = {
   docs: {
     source: {
       code: overview,
+    },
+  },
+};
+
+export const Default = () => _default;
+Default.parameters = {
+  docs: {
+    source: {
+      code: _default,
+    },
+  },
+};
+
+export const Persistent = () => persistent;
+Persistent.parameters = {
+  docs: {
+    source: {
+      code: persistent,
+    },
+  },
+};
+
+export const OnContent = () => onContent;
+OnContent.parameters = {
+  docs: {
+    source: {
+      code: onContent,
     },
   },
 };
