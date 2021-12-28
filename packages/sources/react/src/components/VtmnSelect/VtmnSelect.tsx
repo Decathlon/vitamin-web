37db52cb5f2576e757ad118c3dc0871b1894eaa3
Thibault Poiré <thibault.poire.partner@decathlon.com>
2021-12-28 17:01:07 +0100

    feat: update select component

--- packages/sources/react/src/components/VtmnSelect/VtmnSelect.tsx
+++ packages/sources/react/src/components/VtmnSelect/VtmnSelect.tsx
@@ -1,32 +1,53 @@
 import React from 'react';
+import clsx from 'clsx';
+
+import '@vtmn/css-select';
 
 export interface VtmnSelectProps extends React.ComponentPropsWithRef<'select'> {
   error?: boolean;
+  errorText?: string;
   helperText?: string;
   id: string;
   labelText?: string;
+  options: React.ComponentPropsWithoutRef<'option'>[];
   valid?: boolean;
 }
 
 export const VtmnSelect = ({
   className,
-  disabled = false,
   error = false,
-  helperText,
+  errorText,
   id,
   labelText,
-  placeholder,
-  valid = false,
+  options = [],
   ref,
   ...props
 }: VtmnSelectProps) => {
+  const errorTextId = `error-text-${id}`;
+  const hasErrorText = error && errorText;
+
   return (
-    <>
+    <div className="vtmn-select_container">
       <label htmlFor={id}>{labelText}</label>
-      <select id={id} {...props}>
-        <option></option>
+      <select
+        id={id}
+        className={clsx('vtmn-select', className, {
+          'vtmn-select--error': error,
+        })}
+        aria-describedby={hasErrorText ? errorTextId : undefined}
+        {...props}
+      >
+        {options.map((option) => {
+          return option;
+        })}
       </select>
-    </>
+
+      {hasErrorText && (
+        <p id={errorTextId} className="vtmn-select_error-text">
+          {errorText}
+        </p>
+      )}
+    </div>
   );
 };
 
