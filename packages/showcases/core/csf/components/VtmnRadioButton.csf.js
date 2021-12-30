c4073565b75c124409eb522f1bb95ac1d5649a37
Laurent Thiebault <thiebault.laurent@gmail.com>
2021-12-30 18:23:42 +0100

    docs: figma url

--- packages/showcases/core/csf/components/VtmnRadioButton.csf.js
+++ packages/showcases/core/csf/components/VtmnRadioButton.csf.js
@@ -31,6 +31,6 @@ export const parameters = {
   },
   design: {
     type: 'figma',
-    url: 'https://www.figma.com/file/zDZIyayUlr1yTWrsi7cFoo/?node-id=783%3A9869',
+    url: 'https://www.figma.com/file/zDZIyayUlr1yTWrsi7cFoo/?node-id=359%3A30',
   },
 };
