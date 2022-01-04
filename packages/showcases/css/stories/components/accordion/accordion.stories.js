b237c5d33f25c8405b59f44efd8c8a7e80adb78e
Gaspard MATHON <gaspard.mathon@hotmail.fr>
2022-01-04 11:51:17 +0100

    feat(@vtmn/css): add structure files

---
+++ packages/showcases/css/stories/components/accordion/accordion.stories.js
@@ -0,0 +1,15 @@
+import overview from './examples/overview.html';
+
+export default {
+  title: 'Components / Accordion ',
+  argTypes: { onchange: { action: 'write' } },
+};
+
+export const Overview = () => overview;
+Overview.parameters = {
+  docs: {
+    source: {
+      code: overview,
+    },
+  },
+};
