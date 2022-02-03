80ee71f53677dd9fa6fde2986b19eb9eb8a01050
Thomas Laigneau <thomas.laigneau.partner@decathlon.com>
2022-02-04 00:18:19 +0100

    feat(@vtmn/vue): add VtmnIcon component

---
+++ packages/sources/vue/src/components/VtmnIcon/types.ts
@@ -0,0 +1,10 @@
+export type VtmnIconVariant =
+  | 'default'
+  | 'brand'
+  | 'reversed'
+  | 'positive'
+  | 'information'
+  | 'warning'
+  | 'negative';
+
+export type VtmnIconSize = 16 | 20 | 24 | 32 | 64;
