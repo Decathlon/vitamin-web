3ff0e8c535ced85aff5c6bd5f2b68f4c47d75dd9
Antoine Vendeville <antoine.vendeville@decathlon.com>
2022-02-22 12:08:58 +0100

    feat(svelte): add badge component

---
+++ packages/sources/svelte/src/components/indicators/VtmnBadge/VtmnBadge.svelte
@@ -0,0 +1,35 @@
+<script>
+  import { cn } from '../../../utils/classnames';
+  import { VARIANT } from './enums';
+  /**
+   * The value of the badge
+   * @type {number}
+   */
+  export let value;
+
+  let className;
+  /**
+   * @type {string} Custom classes to apply to the component.
+   */
+  export { className as class };
+
+  /**
+   * The value of the badge
+   * @type {'default' | 'brand' | 'reversed' | 'accent' | 'alert'}
+   */
+  export let variant = VARIANT.DEFAULT;
+
+  $: computedValue = value > 99 ? '99+' : value;
+
+  $: componentClass = cn(
+    'vtmn-badge',
+    variant && `vtmn-badge_variant--${variant}`,
+    className,
+  );
+</script>
+
+<span class={componentClass} {...$$restProps}>{computedValue || ''}</span>
+
+<style lang="css">
+  @import '@vtmn/css-badge';
+</style>
---
+++ packages/sources/svelte/src/components/indicators/VtmnBadge/enums.js
@@ -0,0 +1,7 @@
+export const VARIANT = {
+  DEFAULT: 'default',
+  BRAND: 'brand',
+  REVERSED: 'reversed',
+  ACCENT: 'accent',
+  ALERT: 'alert',
+};
