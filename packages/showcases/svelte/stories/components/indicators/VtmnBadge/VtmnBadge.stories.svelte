8107e18b9a6cb0777f406f9b7fbd0051cfa66aed
Gaspard MATHON <gaspard.mathon@hotmail.fr>
2022-02-23 16:19:45 +0100

    test(@vtmn/svlete): remove multiple stories, clear docs and args with between story and code

--- packages/showcases/svelte/stories/components/indicators/VtmnBadge/VtmnBadge.stories.svelte
+++ packages/showcases/svelte/stories/components/indicators/VtmnBadge/VtmnBadge.stories.svelte
@@ -1,69 +1,29 @@
 <script>
   import { Meta, Template, Story } from '@storybook/addon-svelte-csf';
+  import { VARIANT } from '@vtmn/svelte/src/components/indicators/VtmnBadge/enums.js';
 
   import { VtmnBadge } from '@vtmn/svelte';
-  import docs from './docs.mdx';
-  import {
-    parameters,
-    argTypes,
-  } from '@vtmn/showcase-core/csf/components/indicators/badge.csf';
+  import { parameters } from '@vtmn/showcase-core/csf/components/indicators/badge.csf';
 </script>
 
 <Meta
   title="Components / Indicators / VtmnBadge"
   component={VtmnBadge}
-  {...argTypes}
-  parameters={{
-    ...parameters,
-    docs: {
-      page: docs,
+  argTypes={{
+    variant: {
+      control: 'select',
+      options: [...Object.keys(VARIANT)],
+      defaultValue: VARIANT.DEFAULT,
+    },
+    value: {
+      control: { type: 'range', min: 0, max: 100 },
     },
   }}
+  {parameters}
 />
 
 <Template let:args>
   <VtmnBadge {...args} />
 </Template>
 
-<Story
-  name="Playground"
-  args={{
-    variant: 'default',
-    value: 6,
-  }}
-/>
-
-<Story name="Default">
-  <VtmnBadge variant="default" />
-  <VtmnBadge variant="default" value="6" />
-  <VtmnBadge variant="default" value="100" />
-</Story>
-
-<Story name="Brand">
-  <VtmnBadge variant="brand" />
-  <VtmnBadge variant="brand" value="6" />
-  <VtmnBadge variant="brand" value="100" />
-</Story>
-
-<Story
-  name="Reversed"
-  parameters={{
-    backgrounds: { default: 'primary-reversed' },
-  }}
->
-  <VtmnBadge variant="reversed" />
-  <VtmnBadge variant="reversed" value="6" />
-  <VtmnBadge variant="reversed" value="100" />
-</Story>
-
-<Story name="Accent">
-  <VtmnBadge variant="accent" />
-  <VtmnBadge variant="accent" value="6" />
-  <VtmnBadge variant="accent" value="100" />
-</Story>
-
-<Story name="Alert">
-  <VtmnBadge variant="alert" />
-  <VtmnBadge variant="alert" value="6" />
-  <VtmnBadge variant="alert" value="100" />
-</Story>
+<Story name="Overview" />
