c704a08589a30ad4e14307958faefb1c37d0fb37
Antoine Vendeville <antoine.vendeville@decathlon.com>
2022-02-28 10:21:52 +0100

    feat(svelte): add documentation

--- packages/showcases/svelte/stories/components/indicators/VtmnBadge/VtmnBadge.stories.svelte
+++ packages/showcases/svelte/stories/components/indicators/VtmnBadge/VtmnBadge.stories.svelte
@@ -2,7 +2,6 @@
   import { Meta, Template, Story } from '@storybook/addon-svelte-csf';
 
   import { VtmnBadge } from '@vtmn/svelte';
-  import docs from './docs.mdx';
   import {
     parameters,
     argTypes,
@@ -13,12 +12,7 @@
   title="Components / Indicators / VtmnBadge"
   component={VtmnBadge}
   {...argTypes}
-  parameters={{
-    ...parameters,
-    docs: {
-      page: docs,
-    },
-  }}
+  {...parameters}
 />
 
 <Template let:args>
@@ -32,38 +26,3 @@
     value: 6,
   }}
 />
-
-<Story name="Default">
-  <VtmnBadge variant="default" />
-  <VtmnBadge variant="default" value="6" />
-  <VtmnBadge variant="default" value="100" />
-</Story>
-
-<Story name="Brand">
-  <VtmnBadge variant="brand" />
-  <VtmnBadge variant="brand" value="6" />
-  <VtmnBadge variant="brand" value="100" />
-</Story>
-
-<Story
-  name="Reversed"
-  parameters={{
-    backgrounds: { default: 'primary-reversed' },
-  }}
->
-  <VtmnBadge variant="reversed" />
-  <VtmnBadge variant="reversed" value="6" />
-  <VtmnBadge variant="reversed" value="100" />
-</Story>
-
-<Story name="Accent">
-  <VtmnBadge variant="accent" />
-  <VtmnBadge variant="accent" value="6" />
-  <VtmnBadge variant="accent" value="100" />
-</Story>
-
-<Story name="Alert">
-  <VtmnBadge variant="alert" />
-  <VtmnBadge variant="alert" value="6" />
-  <VtmnBadge variant="alert" value="100" />
-</Story>
