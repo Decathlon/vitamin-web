27f6c526b2241450d6d015aa1a8de9dc0b014674
Laurent Thiebault <thiebault.laurent@gmail.com>
2022-03-01 23:44:59 +0100

    refactor(@vtmn/vue): reorder imports of components

--- packages/sources/vue/src/components/index.ts
+++ packages/sources/vue/src/components/index.ts
@@ -2,18 +2,18 @@ import VtmnButton from './actions/VtmnButton';
 import VtmnLink from './actions/VtmnLink';
 import VtmnTextInput from './forms/VtmnTextInput';
 import VtmnSelect from './forms/VtmnSelect';
+import VtmnLoader from './indicators/VtmnLoader';
 import VtmnCheckbox from './selection-controls/VtmnCheckbox';
 import VtmnRadioButton from './selection-controls/VtmnRadioButton';
 import VtmnToggle from './selection-controls/VtmnToggle';
-import VtmnLoader from './indicators/VtmnLoader';
 
 export {
   VtmnButton,
   VtmnLink,
   VtmnTextInput,
   VtmnSelect,
+  VtmnLoader,
   VtmnCheckbox,
   VtmnRadioButton,
   VtmnToggle,
-  VtmnLoader,
 };
