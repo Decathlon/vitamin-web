ce529da34aec032d343dbb632316bb99546c3801
Laurent Thiebault <thiebault.laurent@gmail.com>
2022-03-02 23:17:10 +0100

    feat(@vtmn/react): add price component

---
+++ packages/sources/react/src/components/indicators/VtmnPrice/VtmnPrice.tsx
@@ -0,0 +1,48 @@
+import * as React from 'react';
+import '@vtmn/css-price/dist/index-with-vars.css';
+import { VtmnPriceVariant, VtmnPriceSize } from './types';
+
+export interface VtmnPriceProps extends React.ComponentPropsWithoutRef<'span'> {
+  /**
+   * The variant of the price.
+   * @defaultValue 'default'
+   */
+  variant?: VtmnPriceVariant;
+
+  /**
+   * The size of the price.
+   * @defaultValue 'medium'
+   */
+  size?: VtmnPriceSize;
+
+  /**
+   * The content to render inside the component.
+   * @defaultValue undefined
+   */
+  children?: React.ReactNode;
+}
+
+export const VtmnPrice = React.forwardRef<HTMLButtonElement, VtmnPriceProps>(
+  (
+    { variant = 'default', size = 'medium', children, className, ...props },
+    ref,
+  ) => {
+    return (
+      <span
+        ref={ref}
+        className={`vtmn-price vtmn-price_variant--${variant} vtmn-price_size--${size} ${
+          className ? className : ''
+        }`}
+        {...props}
+      >
+        {children}
+      </span>
+    );
+  },
+);
+
+const MemoVtmnPrice = React.memo(VtmnPrice);
+
+MemoVtmnPrice.displayName = 'VtmnPrice';
+
+export default MemoVtmnPrice;
---
+++ packages/sources/react/src/components/indicators/VtmnPrice/types.ts
@@ -0,0 +1,3 @@
+export type VtmnPriceVariant = 'default' | 'accent' | 'alert' | 'strikethrough';
+
+export type VtmnPriceSize = 'small' | 'medium' | 'large';
