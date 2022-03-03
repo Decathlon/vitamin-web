99b01360db6057950a7e22b032e34931fa81c1aa
Laurent Thiebault <thiebault.laurent@gmail.com>
2022-03-03 10:44:02 +0100

    feat(@vtmn/vue): add `VtmnPrice` component (#1006)
    * feat(@vtmn/vue): add price component
    
    * Update packages/showcases/svelte/stories/components/indicators/VtmnPrice/VtmnPrice.stories.svelte
    
    Co-authored-by: Gaspard Mathon <gaspard.mathon@hotmail.fr>
    
    * Update packages/showcases/vue/stories/components/indicators/VtmnPrice.stories.js
    
    Co-authored-by: Gaspard Mathon <gaspard.mathon@hotmail.fr>
    
    * Update packages/sources/vue/src/components/indicators/VtmnPrice/VtmnPrice.vue
    
    Co-authored-by: Gaspard Mathon <gaspard.mathon@hotmail.fr>
    
    Co-authored-by: Gaspard Mathon <gaspard.mathon@hotmail.fr>

--- packages/sources/vue/src/components/index.ts
+++ packages/sources/vue/src/components/index.ts
@@ -3,6 +3,7 @@ import VtmnLink from './actions/VtmnLink';
 import VtmnTextInput from './forms/VtmnTextInput';
 import VtmnSelect from './forms/VtmnSelect';
 import VtmnLoader from './indicators/VtmnLoader';
+import VtmnPrice from './indicators/VtmnPrice';
 import VtmnCheckbox from './selection-controls/VtmnCheckbox';
 import VtmnRadioButton from './selection-controls/VtmnRadioButton';
 import VtmnToggle from './selection-controls/VtmnToggle';
@@ -13,6 +14,7 @@ export {
   VtmnTextInput,
   VtmnSelect,
   VtmnLoader,
+  VtmnPrice,
   VtmnCheckbox,
   VtmnRadioButton,
   VtmnToggle,
