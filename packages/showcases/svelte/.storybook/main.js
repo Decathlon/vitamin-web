b25bfbd36772688ada51d37302ca7f0548511c9d
Antoine Vendeville <antoine.vendeville@decathlon.com>
2022-03-06 23:53:10 +0100

    feat(svelte): rollback storybook addon

--- packages/showcases/svelte/.storybook/main.js
+++ packages/showcases/svelte/.storybook/main.js
@@ -4,13 +4,9 @@ module.exports = {
     '../stories/components/**/*.stories.{js,jsx,ts,tsx,mdx,svelte}'
   ],
   addons: [
-    '@storybook/addon-a11y',
-    '@storybook/addon-actions',
-    '@storybook/addon-backgrounds',
-    '@storybook/addon-docs',
-    '@storybook/addon-essentials',
     '@storybook/addon-svelte-csf',
-    '@storybook/addon-viewport',
+    '@storybook/addon-essentials',
+    '@storybook/addon-a11y',
     '@whitespace/storybook-addon-html',
     'storybook-addon-designs',
   ],
