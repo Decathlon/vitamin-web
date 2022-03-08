920791d6cada70c06a36e7e0981013097ba4e855
jb-thery <jb.thery@flameapp.fr>
2022-03-08 14:55:28 +0100

    refactor(VtmnSearch): refactor stories

--- packages/showcases/svelte/stories/components/navigation/VtmnSearch.stories.svelte
+++ packages/showcases/svelte/stories/components/navigation/VtmnSearch.stories.svelte
@@ -8,16 +8,28 @@
   component={VtmnSearch}
   argTypes={{
     variant: {
+      type: { name: 'string', required: true },
       description: 'Color variant of search component.',
+      control: {
+        type: 'select',
+        options: ['default', 'ghost', 'persistent', 'on-content'],
+      },
       defaultValue: 'default',
     },
     disabled: {
+      type: { name: 'boolean', required: false },
       description: 'Disabled state',
       defaultValue: false,
+      control: { type: 'boolean' },
     },
     size: {
+      type: { name: 'string', required: false },
       description: 'Size of input.',
       defaultValue: 'medium',
+      control: {
+        type: 'select',
+        options: ['small', 'medium'],
+      },
     },
   }}
   parameters={{
