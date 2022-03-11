54c41f92c8e21cc852725bb8419824144827d141
yhafidi <youssef.hafidi.partner@decathlon.com>
2022-03-11 14:30:17 +0100

    feat(accordion): code clean accordion review

--- packages/showcases/svelte/stories/components/structure/VtmnAccordion/VtmnAccordion.stories.svelte
+++ packages/showcases/svelte/stories/components/structure/VtmnAccordion/VtmnAccordion.stories.svelte
@@ -1,58 +1,10 @@
 <script>
   import { Meta, Story } from '@storybook/addon-svelte-csf';
   import { VtmnAccordion, VtmnButton } from '@vtmn/svelte';
-  import { parameters } from '@vtmn/showcase-core/csf/components/structure/accordion.csf';
-  const argTypes = {
-    summary: {
-      type: { name: 'string', required: true },
-      description: 'Summary of the accordion',
-      defaultValue: 'Item 1',
-      control: {
-        type: 'text',
-      },
-    },
-    open: {
-      type: { name: 'boolean', required: false },
-      description: 'Define if accorion is open or not',
-      defaultValue: false,
-      control: {
-        type: 'boolean',
-      },
-    },
-    withIconLeft: {
-      type: { name: 'boolean', required: false },
-      description: 'Display left icon on summary',
-      defaultValue: false,
-      control: {
-        type: 'boolean',
-      },
-    },
-    disabled: {
-      type: { name: 'boolean', required: false },
-      description: 'Disable the accordion',
-      defaultValue: false,
-      control: {
-        type: 'boolean',
-      },
-    },
-    class: {
-      type: { name: 'string', required: true },
-      description: 'list of classes of the accordion',
-      defaultValue: 'accordion-stories',
-      control: {
-        type: 'text',
-      },
-    },
-    slot: {
-      type: { name: 'string', required: false },
-      description: 'Description of the accordion',
-      defaultValue: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis esse
-                    aliquam vel quia beatae facere veniam`,
-      control: {
-        type: 'text',
-      },
-    },
-  };
+  import {
+    argTypes,
+    parameters,
+  } from '@vtmn/showcase-core/csf/components/structure/accordion.csf';
 </script>
 
 <Meta
