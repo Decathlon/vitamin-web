d9bb964809d8e1efe3aa71ad5428b3d656891a0d
Youssef HAFIDI <72210827+yhafidi@users.noreply.github.com>
2022-03-11 15:06:12 +0100

    feat(@vtmn/svelte): add `VtmnAccordion` component (#1026)
    * feat(@vtmn/svelte): add VtmnAccordion component
    
    * fix(accordion): code review clean
    
    * feat(accordion): code clean rollup config
    
    * feat(accordion): code clean rollup config
    
    * feat(accordion): code clean index js
    
    * Update packages/sources/svelte/rollup.config.js
    
    Co-authored-by: Laurent Thiebault <thiebault.laurent@gmail.com>
    
    * feat(accordion): code clean accordion review
    
    Co-authored-by: Laurent Thiebault <thiebault.laurent@gmail.com>

---
+++ packages/sources/svelte/src/components/structure/VtmnAccordion/VtmnAccordion.svelte
@@ -0,0 +1,40 @@
+<script>
+  import { cn } from '../../../utils/classnames';
+  /**
+   * @type {boolean} Display left icon on summary.
+   */
+  export let withIconLeft = false;
+  /**
+   * @type {string} Summary of the accordion.
+   */
+  export let summary;
+  /**
+   * @type {boolean} Disable the accordion.
+   */
+  export let disabled = false;
+  /**
+   * @type {boolean} Open the accordion.
+   */
+  export let open = false;
+  let className;
+  /**
+   * @type {string} Custom classes to apply to the component.
+   */
+  export { className as class };
+  $: componentClass = cn(
+    'vtmn-accordion',
+    withIconLeft ? 'vtmn-accordion--with-icon-left' : '',
+    className,
+  );
+</script>
+
+<details class={componentClass} aria-disabled={disabled} {open}>
+  <summary>{summary}</summary>
+  <div class="vtmn-accordion_content">
+    <slot />
+  </div>
+</details>
+
+<style lang="css">
+  @import '@vtmn/css-accordion';
+</style>
