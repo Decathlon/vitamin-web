1016dd9383c6e4f122f82bcd7f592a22541ce823
jb-thery <jb.thery@flameapp.fr>
2022-03-15 17:11:29 +0100

    refactor(VtmnNavbar): use slots to mak navbar more generic

--- packages/showcases/core/csf/components/navigation/navbar.csf.js
+++ packages/showcases/core/csf/components/navigation/navbar.csf.js
@@ -12,25 +12,4 @@ export const argTypes = {
     defaultValue: false,
     control: { type: 'text' },
   },
-
-  navMenu: {
-    type: { name: 'boolean', required: false },
-    description: 'Diplay right nav menu button',
-    defaultValue: false,
-    control: { type: 'boolean' },
-  },
-
-  logo: {
-    type: { name: 'boolean', required: false },
-    description: 'Diplay Decathlon logo',
-    defaultValue: false,
-    control: { type: 'boolean' },
-  },
-
-  search: {
-    type: { name: 'boolean', required: false },
-    description: 'Diplay search input',
-    defaultValue: false,
-    control: { type: 'boolean' },
-  },
 };
--- packages/sources/svelte/src/components/navigation/VtmnNavbar/VtmnNavbar.spec.js
+++ packages/sources/svelte/src/components/navigation/VtmnNavbar/VtmnNavbar.spec.js
@@ -8,6 +8,59 @@ describe('<VtmnNavbar />', () => {
     expect(container).toBeInTheDocument();
   });
 
+  test('Render correctly with all slots', () => {
+    const { container } = render(VtmnNavbar, {
+      slots: {
+        'left-nav': '<div>left-nav</div>',
+        logo: '<h1>logo</h1>',
+        'middle-area': '<div>middle-area</div>',
+        'right-nav': '<div>right-nav</div>',
+      },
+    });
+
+    expect(container).toBeInTheDocument();
+  });
+
+  test('Render correctly with left-nav slot', () => {
+    const { container } = render(VtmnNavbar, {
+      slots: {
+        'left-nav': '<div>left-nav</div>',
+      },
+    });
+
+    expect(container).toBeInTheDocument();
+  });
+
+  test('Render correctly with logo slot', () => {
+    const { container } = render(VtmnNavbar, {
+      slots: {
+        logo: '<h1>logo</h1>',
+      },
+    });
+
+    expect(container).toBeInTheDocument();
+  });
+
+  test('Render correctly with middle-area slot', () => {
+    const { container } = render(VtmnNavbar, {
+      slots: {
+        'middle-area': '<div>middle-area</div>',
+      },
+    });
+
+    expect(container).toBeInTheDocument();
+  });
+
+  test('Render correctly with right-nav slot', () => {
+    const { container } = render(VtmnNavbar, {
+      slots: {
+        'right-nav': '<div>right-nav</div>',
+      },
+    });
+
+    expect(container).toBeInTheDocument();
+  });
+
   test('Can add custom css classes', () => {
     const { container } = render(VtmnNavbar, {
       className: 'test-class',
@@ -16,43 +69,9 @@ describe('<VtmnNavbar />', () => {
     expect(container.getElementsByClassName(`test-class`).length).toBe(1);
   });
 
-  test('Render navMenu correctly', () => {
-    const { container } = render(VtmnNavbar, { navMenu: true });
-
-    expect(
-      container.getElementsByClassName('vtmn-navbar_left-navigation').length,
-    ).toBe(1);
-  });
-
-  test('Render logo correctly', () => {
-    const { container } = render(VtmnNavbar, { logo: true });
-
-    expect(container.getElementsByClassName('vtmn-navbar_logo').length).toBe(1);
-  });
-
-  test('Render search correctly', () => {
-    const { container } = render(VtmnNavbar, { search: true });
-
-    expect(
-      container.getElementsByClassName('vtmn-navbar_middle-area').length,
-    ).toBe(1);
-  });
-
   test('Can have custom props', async () => {
     const { container } = render(VtmnNavbar, { id: 'test' });
 
     expect(container.getElementsByTagName('nav')[0].id).toBe('test');
-  });
-
-  test('ToggleMenu event is dispatched', async () => {
-    const handleClick = jest.fn();
-    const { container, component } = render(VtmnNavbar, {
-      navMenu: true,
-    });
-
-    component.$on('toggleMenu', handleClick);
-
-    await fireEvent.click(container.getElementsByTagName('button')[0]);
-    expect(handleClick).toHaveBeenCalledTimes(1);
   });
 });
