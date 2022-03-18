c962ec7ece9615335be7aec9b5aac43499e1a4dc
Youssef HAFIDI <72210827+yhafidi@users.noreply.github.com>
2022-03-18 16:20:06 +0100

    feat(@vtmn/svelte): add `VtmnSkeleton` component (#1047)
    * feat(@vtmn/svelte): add VtmnSkeleton component
    
    * feat(VtmnSkeleton): add TU

--- packages/showcases/core/csf/components/structure/skeleton.csf.js
+++ packages/showcases/core/csf/components/structure/skeleton.csf.js
@@ -7,3 +7,25 @@ export const parameters = {
     url: 'https://www.figma.com/file/zDZIyayUlr1yTWrsi7cFoo/?node-id=3451%3A10738',
   },
 };
+
+export const argTypes = {
+  width: {
+    type: { name: 'number', required: false },
+    description: 'Width of the skeleton',
+    defaultValue: 30,
+    control: {
+      type: 'range',
+      min: 0,
+      max: 100,
+    },
+  },
+  shape: {
+    type: { name: 'string', required: false },
+    description: 'Define the type of shape',
+    defaultValue: 'line',
+    control: {
+      type: 'select',
+      options: ['line', 'avatar'],
+    },
+  },
+};
---
+++ packages/sources/svelte/src/components/structure/VtmnSkeleton/VtmnSkeleton.svelte
@@ -0,0 +1,43 @@
+<script>
+  import { cn } from '../../../utils/classnames';
+  import { VTMN_SKELETON_SHAPE } from './enums';
+  import { objectToStyle } from '../../../utils/style';
+
+  /**
+   * @type {number} width to apply to the skeleton
+   * @defaultValue 0
+   */
+  export let width = 0;
+
+  /**
+   * @type {'line' | 'avatar' }  variant of the shape.
+   * @defaultValue line
+   */
+  export let shape = VTMN_SKELETON_SHAPE.LINE;
+
+  /**
+   * @type {string} Custom classes to apply to the component.
+   */
+  let className = '';
+
+  export { className as class };
+  $: componentClass = cn(
+    'vtmn-skeleton',
+    shape && `vtmn-skeleton_${shape}`,
+    width && `skeleton-width`,
+    className,
+  );
+
+  $: componentStyle = objectToStyle({
+    '--skeleton-width': `${width}%`,
+  });
+</script>
+
+<span class={componentClass} style={componentStyle} />
+
+<style lang="css">
+  @import '@vtmn/css-skeleton';
+  .skeleton-width {
+    width: var(--skeleton-width, 0);
+  }
+</style>
