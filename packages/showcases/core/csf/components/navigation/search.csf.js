7cc6b0b3b77a74522b457cc124179bf4df825403
Lucas Martin <20931885+0x2A-git@users.noreply.github.com>
2022-03-21 10:45:33 +0100

    feat(@vtmn/react): add `VtmnSearch` component (#1031)
    * feat(@vtmn/react): add search component
    
    * feat(@vtmn/react): add search showcase
    
    * feat(@vtmn/react): add missing value prop
    
    * refactor(@vtmn/react): rename search folder

--- packages/showcases/core/csf/components/navigation/search.csf.js
+++ packages/showcases/core/csf/components/navigation/search.csf.js
@@ -10,27 +10,45 @@ export const parameters = {
 
 export const argTypes = {
   variant: {
-    type: { name: 'string', required: true },
-    description: 'Color variant of search component.',
+    type: { name: 'string', required: false },
+    description: 'The variant of the search bar.',
+    defaultValue: 'default',
     control: {
       type: 'select',
       options: ['default', 'persistent', 'on-content'],
     },
-    defaultValue: 'default',
-  },
-  disabled: {
-    type: { name: 'boolean', required: false },
-    description: 'Disabled state',
-    defaultValue: false,
-    control: { type: 'boolean' },
   },
   size: {
     type: { name: 'string', required: false },
-    description: 'Size of input.',
+    description: 'The size of the search bar.',
     defaultValue: 'medium',
     control: {
       type: 'select',
       options: ['small', 'medium'],
     },
+  },
+  disabled: {
+    type: { name: 'boolean', required: false },
+    description: 'Whether the search bar is disabled.',
+    table: {
+      type: {
+        summary: 'boolean',
+      },
+      defaultValue: { summary: 'false' },
+    },
+    defaultValue: false,
+    control: { type: 'boolean' },
+  },
+  placeholder: {
+    type: { name: 'string', required: false },
+    description: 'The placeholder of the search bar.',
+    defaultValue: 'Search',
+    control: { type: 'text' },
+  },
+  value: {
+    type: { name: 'string', required: false },
+    description: 'The value of the search bar.',
+    defaultValue: undefined,
+    control: { type: 'text' },
   },
 };
