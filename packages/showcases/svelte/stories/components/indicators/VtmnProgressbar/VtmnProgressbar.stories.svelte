66e5a0b385c06536ebde80f4873c38d65e77d797
Antoine Vendeville <antoine.vendeville@decathlon.com>
2022-03-23 23:39:02 +0100

    feat(@vtmn/svelte): move argType into story

--- packages/showcases/svelte/stories/components/indicators/VtmnProgressbar/VtmnProgressbar.stories.svelte
+++ packages/showcases/svelte/stories/components/indicators/VtmnProgressbar/VtmnProgressbar.stories.svelte
@@ -2,8 +2,81 @@
   import { Meta, Template, Story } from '@storybook/addon-svelte-csf';
   import { VtmnProgressbar } from '@vtmn/svelte';
   import { parameters } from '@vtmn/showcase-core/csf/components/indicators/progressbar.csf';
-  import { argTypes } from './progressbar.csf';
   import README from '@vtmn/svelte/src/components/indicators/VtmnProgressbar/README.md';
+  const argTypes = {
+    variant: {
+      type: { name: 'string', required: true },
+      description: 'Variant of the loader.',
+      defaultValue: 'linear',
+      control: {
+        type: 'radio',
+        options: ['linear', 'circular'],
+      },
+    },
+    size: {
+      type: { name: 'string', required: false },
+      description: 'The size of the loader.',
+      defaultValue: 'medium',
+      table: {
+        type: {
+          summary: 'string',
+        },
+        defaultValue: { summary: 'medium' },
+      },
+      control: {
+        type: 'radio',
+        options: ['small', 'medium', 'large'],
+      },
+    },
+    label: {
+      type: { name: 'string', required: false },
+      description: 'Label displayed for the loader',
+      control: {
+        type: 'text',
+      },
+    },
+    progress: {
+      type: { name: 'number', required: false },
+      description: 'Value of the progressbar',
+      defaultValue: 30,
+      control: {
+        type: 'range',
+        min: 0,
+        max: 100,
+      },
+    },
+    indeterminate: {
+      type: { name: 'boolean', required: false },
+      description: 'Set the loader to indeterminate',
+      defaultValue: false,
+      control: {
+        type: 'boolean',
+      },
+    },
+    track: {
+      type: { name: 'boolean', required: false },
+      description: 'Display track path',
+      defaultValue: false,
+      control: {
+        type: 'boolean',
+      },
+    },
+    img: {
+      type: { name: 'string', required: false },
+      description: 'Image source for circle loader',
+      defaultValue: '',
+      control: {
+        type: 'text',
+      },
+    },
+    imgAlt: {
+      type: { name: 'string', required: false },
+      description: 'Alternative text for the image.',
+      control: {
+        type: 'text',
+      },
+    },
+  };
 
   const img =
     'https://storage.googleapis.com/dkt-design-cdn/images/landscape-placeholder.jpg';
