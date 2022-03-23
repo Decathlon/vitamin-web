99fbaa81198b99cc2f38a715747695cede6895d1
Antoine Vendeville <antoine.vendeville@decathlon.com>
2022-03-23 23:34:36 +0100

    feat(@vtmn/svelte): add argTypes on story

--- packages/showcases/svelte/stories/components/forms/VtmnSelect/VtmnSelect.stories.svelte
+++ packages/showcases/svelte/stories/components/forms/VtmnSelect/VtmnSelect.stories.svelte
@@ -2,7 +2,50 @@
   import { Meta, Template, Story } from '@storybook/addon-svelte-csf';
   import { VtmnSelect } from '@vtmn/svelte';
   import { parameters } from '@vtmn/showcase-core/csf/components/forms/select.csf';
-  import { argTypes } from './select.csf';
+  const argTypes = {
+    disabled: {
+      type: { name: 'boolean', required: false },
+      description: 'The disabled state of the select.',
+      defaultValue: false,
+      control: { type: 'boolean' },
+    },
+    error: {
+      type: { name: 'string', required: false },
+      description: 'The error message displayed under the select.',
+      defaultValue: '',
+      control: { type: 'text' },
+    },
+    id: {
+      type: { name: 'string', required: true },
+      description: 'The id of the select',
+      defaultValue: 'vtmn-select',
+      control: { type: 'text' },
+    },
+    name: {
+      type: { name: 'string', required: true },
+      description: 'The name of the select',
+      defaultValue: 'sports',
+      control: { type: 'text' },
+    },
+    label: {
+      type: { name: 'string', required: true },
+      description: 'The label of the select.',
+      defaultValue: 'Label',
+      control: { type: 'text' },
+    },
+    autocomplete: {
+      type: { name: 'boolean', required: false },
+      description: 'Enable autocompletion on select',
+      defaultValue: false,
+      control: { type: 'boolean' },
+    },
+    value: {
+      type: { name: 'object', required: true },
+      description: 'Selected value',
+      defaultValue: '',
+      control: { type: 'object' },
+    },
+  };
   let value = '';
 </script>
 
