ff436215aa61f4bbba57b0bfdacf654afdd7c62b
Amine Daï <amine.dai@free.fr>
2022-03-25 15:16:26 +0100

    fix(@vtmn/react): utils method and button component (#1073)
    * fix(@vtmn/react): utils object values to string
    
    * fix(@vtmn/react): button disabled property

--- packages/sources/react/src/utils/object.ts
+++ packages/sources/react/src/utils/object.ts
@@ -1,2 +1,18 @@
-export const objectValuesToString = (object: object) =>
-  Object.keys(object).map((key) => object[key].toString());
+export const isObject = (object: object) => {
+  return (
+    typeof object === 'object' && !Array.isArray(object) && object !== null
+  );
+};
+
+export const objectValuesToString = (object: object) => {
+  if (!isObject(object)) {
+    return {};
+  }
+
+  return JSON.parse(JSON.stringify(object), (_key, value) => {
+    if (typeof value === 'boolean' || typeof value === 'number') {
+      return value.toString();
+    }
+    return value;
+  });
+};
