13343e52b477e3b835439f35f552fe969e8d0687
Antoine Vendeville <antoine.vendeville@decathlon.com>
2022-03-26 19:10:28 +0100

    test(@vtmn/svelte): add VtmnPrice tests

--- packages/sources/svelte/src/components/indicators/VtmnPrice/test/VtmnPrice.spec.js
+++ packages/sources/svelte/src/components/indicators/VtmnPrice/test/VtmnPrice.spec.js
@@ -1,8 +1,52 @@
 import '@testing-library/jest-dom';
 
-import { render, fireEvent } from '@testing-library/svelte';
-import VtmnPrice from '../VtmnPrice.svelte';
+import { render } from '@testing-library/svelte';
+import VtmnPrice from './VtmnPriceWithSlots.svelte';
 
 describe('VtmnPrice', () => {
-  test('', () => {});
+  const getPrice = (container) =>
+    container.getElementsByClassName('vtmn-price')[0];
+
+  test('Should display vtmn-price with medium size and default variant', () => {
+    const { container } = render(VtmnPrice);
+    expect(getPrice(container)).toHaveClass(
+      'vtmn-price_size--medium',
+      'vtmn-price_variant--default',
+    );
+    expect(getPrice(container)).toBeVisible();
+  });
+  test('Should apply size', () => {
+    const { container } = render(VtmnPrice, { size: 'large' });
+    expect(getPrice(container)).toHaveClass(
+      'vtmn-price_size--large',
+      'vtmn-price_variant--default',
+    );
+    expect(getPrice(container)).toBeVisible();
+  });
+  test('Should apply variant', () => {
+    const { container } = render(VtmnPrice, { variant: 'alert' });
+    expect(getPrice(container)).toHaveClass(
+      'vtmn-price_size--medium',
+      'vtmn-price_variant--alert',
+    );
+    expect(getPrice(container)).toBeVisible();
+  });
+  test('Should apply custom class on component', () => {
+    const { container } = render(VtmnPrice, { class: 'custom-class' });
+    expect(getPrice(container)).toHaveClass('custom-class');
+    expect(getPrice(container)).toBeVisible();
+  });
+  test('Should apply aria-label on component', () => {
+    const { container } = render(VtmnPrice, {
+      'aria-label': 'custom aria label',
+    });
+    expect(getPrice(container)).toHaveAttribute(
+      'aria-label',
+      'custom aria label',
+    );
+  });
+  test('Should display the slot', () => {
+    const { getByText } = render(VtmnPrice);
+    expect(getByText('100€')).toBeVisible();
+  });
 });
