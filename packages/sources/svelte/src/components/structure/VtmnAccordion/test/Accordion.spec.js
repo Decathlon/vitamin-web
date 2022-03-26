c523d03015a241c43aca91b20a48f675e3e4566f
Antoine Vendeville <antoine.vendeville@decathlon.com>
2022-03-26 22:57:19 +0100

    test(@vtmn/svelte): add VtmnAccordion tests

--- packages/sources/svelte/src/components/structure/VtmnAccordion/test/Accordion.spec.js
+++
@@ -1,8 +0,0 @@
-import '@testing-library/jest-dom';
-
-import { render, fireEvent } from '@testing-library/svelte';
-import VtmnAccordion from '../VtmnAccordion.svelte';
-
-describe('VtmnAccordion', () => {
-  test('', () => {});
-});
