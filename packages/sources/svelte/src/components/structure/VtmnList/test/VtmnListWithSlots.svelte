b8251db99364dca8dfec3ab3479343795b50ef20
Antoine Vendeville <antoine.vendeville@decathlon.com>
2022-03-27 12:21:21 +0200

    test(@vtmn/svelte): add VtmnList tests

---
+++ packages/sources/svelte/src/components/structure/VtmnList/test/VtmnListWithSlots.svelte
@@ -0,0 +1,13 @@
+<script>
+  import VtmnList from '../VtmnList.svelte';
+  import VtmnListItem from '../../VtmnListItem/VtmnListItem.svelte';
+</script>
+
+<VtmnList {...$$restProps}>
+  <VtmnListItem index="1">
+    <svelte:fragment slot="text">Item 1</svelte:fragment>
+  </VtmnListItem>
+  <VtmnListItem index="2">
+    <svelte:fragment slot="text">Item 2</svelte:fragment>
+  </VtmnListItem>
+</VtmnList>
