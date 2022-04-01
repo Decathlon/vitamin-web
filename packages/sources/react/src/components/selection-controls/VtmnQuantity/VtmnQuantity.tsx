8cb7d800e4e62e0dbb4781db3a0bb001d58a518d
0x2A-git <zozosteam@protonmail.com>
2022-04-01 11:57:27 +0200

    feat(@vtmn/react): forward ref to the input

--- packages/sources/react/src/components/selection-controls/VtmnQuantity/VtmnQuantity.tsx
+++ packages/sources/react/src/components/selection-controls/VtmnQuantity/VtmnQuantity.tsx
@@ -67,86 +67,95 @@ export interface VtmnQuantityProps
   onChange?: (newQuantity: number, action: string) => void;
 }
 
-export const VtmnQuantity = ({
-  id,
-  value = 0,
-  step = 1,
-  label = undefined,
-  disabled = false,
-  min = 0,
-  max = Infinity,
-  error = undefined,
-  onChange = undefined,
-  className,
-  ...props
-}: VtmnQuantityProps) => {
-  const [quantity, setQuantity] = React.useState(value);
+export const VtmnQuantity = React.forwardRef<
+  HTMLInputElement,
+  VtmnQuantityProps
+>(
+  (
+    {
+      id,
+      value = 0,
+      step = 1,
+      label = undefined,
+      disabled = false,
+      min = 0,
+      max = Infinity,
+      error = undefined,
+      onChange = undefined,
+      className,
+      ...props
+    }: VtmnQuantityProps,
+    ref,
+  ) => {
+    const [quantity, setQuantity] = React.useState(value);
 
-  return (
-    <div className={clsx('vtmn-quantity', className)} {...props}>
-      {label && <label htmlFor={id}>{label}</label>}
+    return (
+      <div className={clsx('vtmn-quantity', className)} {...props}>
+        {label && <label htmlFor={id}>{label}</label>}
 
-      <div className="vtmn-quantity_content">
-        <VtmnButton
-          variant="secondary"
-          disabled={disabled || quantity <= min}
-          iconAlone={'subtract-fill'}
-          onClick={() =>
-            setQuantity((oldQuantity) => {
-              const newQuantity = Math.max(min, oldQuantity - step);
+        <div className="vtmn-quantity_content">
+          <VtmnButton
+            variant="secondary"
+            disabled={disabled || quantity <= min}
+            iconAlone={'subtract-fill'}
+            onClick={() =>
+              setQuantity((oldQuantity) => {
+                const newQuantity = Math.max(min, oldQuantity - step);
 
-              onChange && onChange(newQuantity, 'subtract');
+                onChange && onChange(newQuantity, 'subtract');
 
-              return newQuantity;
-            })
-          }
-          aria-label="subtract"
-        />
-        <input
-          type="number"
-          disabled={disabled}
-          id={id}
-          min={min}
-          max={max}
-          step={step}
-          value={quantity}
-          aria-describedby={`quantity-helper-${id}`}
-          onChange={(event) => {
-            const newQuantity = isNaN(event.target.valueAsNumber)
-              ? min
-              : event.target.valueAsNumber;
+                return newQuantity;
+              })
+            }
+            aria-label="subtract"
+          />
+          <input
+            type="number"
+            ref={ref}
+            disabled={disabled}
+            id={id}
+            min={min}
+            max={max}
+            step={step}
+            value={quantity}
+            aria-describedby={`quantity-helper-${id}`}
+            onChange={(event) => {
+              const newQuantity = isNaN(event.target.valueAsNumber)
+                ? min
+                : event.target.valueAsNumber;
 
-            onChange && onChange(newQuantity, 'edit');
+              onChange && onChange(newQuantity, 'edit');
 
-            setQuantity(newQuantity);
-          }}
-          aria-invalid={error ? true : undefined}
-        />
-        <VtmnButton
-          variant="secondary"
-          disabled={disabled || quantity >= max}
-          iconAlone={'add-fill'}
-          onClick={() =>
-            setQuantity((oldQuantity) => {
-              const newQuantity = Math.min(max, oldQuantity + step);
+              setQuantity(newQuantity);
+            }}
+            aria-invalid={error ? true : undefined}
+          />
+          <VtmnButton
+            variant="secondary"
+            disabled={disabled || quantity >= max}
+            iconAlone={'add-fill'}
+            onClick={() =>
+              setQuantity((oldQuantity) => {
+                const newQuantity = Math.min(max, oldQuantity + step);
 
-              onChange && onChange(newQuantity, 'add');
+                onChange && onChange(newQuantity, 'add');
 
-              return newQuantity;
-            })
-          }
-          aria-label="add"
-        />
+                return newQuantity;
+              })
+            }
+            aria-label="add"
+          />
+        </div>
+
+        {error && (
+          <p id={`quantity-helper-${id}`} className="vtmn-quantity_error-text">
+            {error}
+          </p>
+        )}
       </div>
-
-      {error && (
-        <p id={`quantity-helper-${id}`} className="vtmn-quantity_error-text">
-          {error}
-        </p>
-      )}
-    </div>
-  );
-};
+    );
+  },
+);
 
 const MemoVtmnQuantity = React.memo(VtmnQuantity);
 
