d246dfdb5f061c4de0354e85ae33549d4059aa27
Laurent Thiebault <thiebault.laurent@gmail.com>
2022-04-11 14:56:20 +0200

    refactor(@vtmn/vue): stories

--- packages/showcases/vue/stories/components/selection-controls/VtmnQuantity/VtmnQuantity.stories.js
+++ packages/showcases/vue/stories/components/selection-controls/VtmnQuantity/VtmnQuantity.stories.js
@@ -1,63 +1,23 @@
+import { ref } from 'vue';
 import { VtmnQuantity } from '@vtmn/vue';
-import { parameters } from '@vtmn/showcase-core/csf/components/selection-controls/quantity.csf';
+import {
+  parameters,
+  argTypes,
+} from '@vtmn/showcase-core/csf/components/selection-controls/quantity.csf';
+
+const { id, value, ...restOfArgTypes } = argTypes;
 
 export default {
   title: 'Components / Selection controls / VtmnQuantity',
   component: VtmnQuantity,
   argTypes: {
+    ...restOfArgTypes,
     identifier: {
       type: { name: 'string', required: true },
       description: 'Id of the input.',
       defaultValue: 'quantity-story',
       control: {
         type: 'text',
-      },
-    },
-    label: {
-      type: { name: 'string', required: true },
-      description: 'Label of the quantity input',
-      defaultValue: 'Label',
-      control: {
-        type: 'text',
-      },
-    },
-    disabled: {
-      type: { name: 'boolean', required: false },
-      description: 'Disable the input',
-      defaultValue: false,
-      control: {
-        type: 'boolean',
-      },
-    },
-    min: {
-      type: { name: 'number', required: false },
-      description: 'Minimum value of the input',
-      defaultValue: 0,
-      control: {
-        type: 'number',
-      },
-    },
-    max: {
-      type: { name: 'number', required: false },
-      description: 'Maximum value of the input',
-      control: {
-        type: 'number',
-      },
-    },
-    error: {
-      type: { name: 'string', required: false },
-      description: 'Error display to the input',
-      control: {
-        type: 'text',
-      },
-    },
-    step: {
-      type: { name: 'number', required: false },
-      description: 'Step of the input',
-      defaultValue: 1,
-      control: {
-        type: 'number',
-        min: 0,
       },
     },
   },
@@ -67,9 +27,14 @@ export default {
 const Template = (args) => ({
   components: { VtmnQuantity },
   setup() {
-    return { args };
+    let quantitySelected = ref(0);
+
+    return {
+      quantitySelected,
+      args,
+    };
   },
-  template: `<VtmnQuantity v-bind="args"/>`,
+  template: `<VtmnQuantity v-model="quantitySelected" v-bind="args"/>`,
 });
 
 export const Overview = Template.bind({});
