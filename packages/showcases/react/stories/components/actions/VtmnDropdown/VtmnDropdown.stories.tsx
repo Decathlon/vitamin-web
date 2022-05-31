eefc6968279002fa6335605b05bc6ddd66420005
Gaspard MATHON <gaspard.mathon@hotmail.fr>
2022-05-31 11:27:51 +0200

    feat(@vtmn/react): add VtmnDropdown component

--- packages/showcases/react/stories/components/actions/VtmnDropdown/VtmnDropdown.stories.tsx
+++ packages/showcases/react/stories/components/actions/VtmnDropdown/VtmnDropdown.stories.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { VtmnDropdown } from '@vtmn/react';
+import { VtmnDropdown, VtmnDropdownItem } from '@vtmn/react';
 import { Meta, Story } from '@storybook/react';
 import {
   argTypes,
@@ -11,22 +11,54 @@ export default {
   component: VtmnDropdown,
   argTypes: {
     ...argTypes,
-    children: {
+    onChange: {
       type: { name: 'string', required: false },
-      description: 'The content to render inside the component.',
-      defaultValue: 'Button',
+      description: 'Called when item has changed',
       control: {
-        type: 'text',
+        type: 'function',
       },
     },
   },
   parameters,
 } as Meta;
 
-const Template: Story = (args) => (
-  <div style={{ width: 400, display: 'flex', justifyContent: 'center' }}>
-    <VtmnDropdown {...args} />
-  </div>
-);
+const Template: Story = (args) => {
+  return (
+    <div style={{ width: 400, display: 'flex', justifyContent: 'center' }}>
+      <VtmnDropdown {...args}>
+        <VtmnDropdownItem
+          id="dropdown-0-opt-1"
+          name="dropdown-0"
+          value="item 1"
+        >
+          Dropdown Item 1
+        </VtmnDropdownItem>
+        <VtmnDropdownItem
+          id="dropdown-0-opt-2"
+          name="dropdown-0"
+          value="item 2"
+          icon="user-line"
+        >
+          Dropdown Item 2
+        </VtmnDropdownItem>
+        <VtmnDropdownItem
+          id="dropdown-0-opt-3"
+          name="dropdown-0"
+          value="item 3"
+          divider="true"
+        >
+          Dropdown Item 3
+        </VtmnDropdownItem>
+        <VtmnDropdownItem
+          id="dropdown-0-opt-4"
+          name="dropdown-0"
+          value="item 4"
+        >
+          Dropdown Item 4
+        </VtmnDropdownItem>
+      </VtmnDropdown>
+    </div>
+  );
+};
 
 export const Overview = Template.bind({});
--- packages/sources/react/src/components/actions/VtmnDropdown/VtmnDropdownItem.tsx
+++ packages/sources/react/src/components/actions/VtmnDropdown/VtmnDropdownItem.tsx
@@ -5,13 +5,7 @@ import { VtmnDivider } from '../../structure/VtmnDivider/VtmnDivider';
 import { VtmnIcon } from '../../../guidelines/iconography/VtmnIcon';
 
 export interface VtmnDropdownItemProps
-  extends React.ComponentPropsWithoutRef<'input'> {
-  /**
-   * Whether the dropdown item is selected.
-   * @defaultValue false
-   */
-  selected?: boolean;
-
+  extends Omit<React.ComponentPropsWithoutRef<'input'>, 'onChange'> {
   /**
    * Whether there is a divider at the end of the dropdown item.
    * @defaultValue false
@@ -29,28 +23,55 @@ export interface VtmnDropdownItemProps
    * @defaultValue undefined
    */
   children?: React.ReactNode;
+
+  /**
+   * Called when item has changed
+   * @type {void}
+   * @defaultValue undefined
+   */
+  onChange?: () => void;
 }
 
-export const VtmnDropdownItem = ({
-  selected = false,
-  divider = false,
-  icon,
-  children,
-  ...props
-}: VtmnDropdownItemProps) => {
-  return (
-    <React.Fragment>
-      <input type="checkbox" {...props} />
-      <label htmlFor={props['id'] ?? undefined}>
-        {icon ? <VtmnIcon value={icon} /> : null}
-        {children}
-      </label>
-      {divider ? (
-        <VtmnDivider orientation="horizontal" role="separator" />
-      ) : null}
-    </React.Fragment>
-  );
-};
+export const VtmnDropdownItem = React.forwardRef<
+  HTMLInputElement,
+  VtmnDropdownItemProps
+>(
+  (
+    {
+      divider = false,
+      icon,
+      children,
+      onChange = undefined,
+      ...props
+    }: VtmnDropdownItemProps,
+    ref,
+  ) => {
+    const [checked, setChecked] = React.useState(false);
+
+    return (
+      <React.Fragment>
+        <input
+          type="checkbox"
+          ref={ref}
+          checked={checked}
+          onChange={() =>
+            setChecked(() => {
+              return !checked;
+            })
+          }
+          {...props}
+        />
+        <label htmlFor={props['id'] ?? undefined}>
+          {icon ? <VtmnIcon value={icon} /> : null}
+          {children}
+        </label>
+        {divider ? (
+          <VtmnDivider orientation="horizontal" role="separator" />
+        ) : null}
+      </React.Fragment>
+    );
+  },
+);
 
 const MemoVtmnDropdownItem = React.memo(VtmnDropdownItem);
 
