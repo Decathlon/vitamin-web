5924cab9a65ed7b49d5513497ddff00b8be1eea6
Gaspard MATHON <gaspard.mathon@hotmail.fr>
2022-06-13 11:22:44 +0200

    feat(@vtmn/css): remove console.log

--- packages/showcases/css/stories/components/actions/dropdown/examples/demo.runscript.js
+++ packages/showcases/css/stories/components/actions/dropdown/examples/demo.runscript.js
@@ -2,39 +2,27 @@ window.addEventListener('DOMContentLoaded', () => {
   let firstItem = document.querySelector('#dropdown-demo-opt-1');
   let lastItem = document.querySelector('#dropdown-demo-opt-6');
 
-  console.log('firstItem', firstItem);
-  console.log('lastItem', lastItem);
-
   let groupItem = document.querySelector('#dropdown-items');
-
-  console.log('groupItem', groupItem);
 
   let firstFocusTrap = document.querySelector('#block-focus-trap-start');
   let lastFocusTrap = document.querySelector('#block-focus-trap-end');
 
-  console.log('firstFocusTrap', firstFocusTrap);
-  console.log('lastFocusTrap', lastFocusTrap);
-
   groupItem.addEventListener('focus', () => {
-    console.log('groupItem focus');
     firstItem.focus();
   });
 
   groupItem.addEventListener('keydown', function (e) {
     if (e.key === 'Escape') {
-      console.log('groupItem keydown Escape');
       document.querySelector('#dropdown-details').open = false;
       document.querySelector('#dropdown-details > summary').focus();
     }
   });
 
   firstFocusTrap.addEventListener('focus', () => {
-    console.log('firstFocusTrap focused');
     lastItem.focus();
   });
 
   lastFocusTrap.addEventListener('focus', () => {
-    console.log('lastFocusTrap focused');
     firstItem.focus();
   });
 });
