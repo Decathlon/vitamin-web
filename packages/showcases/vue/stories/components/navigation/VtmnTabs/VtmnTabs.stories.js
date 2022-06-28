1d5406ebea11dda82bb8fed3a327254debec862c
Gaspard MATHON <gaspard.mathon@hotmail.fr>
2022-06-28 15:49:00 +0200

    feat(@vtmn/vue): tabs refacto showcase

--- packages/showcases/vue/stories/components/navigation/VtmnTabs/VtmnTabs.stories.js
+++ packages/showcases/vue/stories/components/navigation/VtmnTabs/VtmnTabs.stories.js
@@ -18,9 +18,9 @@ const Template = (args) => ({
   },
   template: `<div style="width: 800px; display: flex; justify-content: center">
       <VtmnTabs aria-label="Tabs list" v-bind="args">
-        <VtmnTabsItem href="">Tab Item 1</VtmnTabsItem>
+        <VtmnTabsItem icon="heart-line" href="">Tab Item 1</VtmnTabsItem>
         <VtmnTabsItem icon="heart-line" href="">Tab Item 2</VtmnTabsItem>
-        <VtmnTabsItem badgeValue="99" href="">Tab Item 3</VtmnTabsItem>
+        <VtmnTabsItem icon="heart-line" badgeValue="99" href="">Tab Item 3</VtmnTabsItem>
         <VtmnTabsItem icon="heart-line" badgeValue="99" href="">Tab Item 4</VtmnTabsItem>
       </VtmnTabs>
     </div>`,
