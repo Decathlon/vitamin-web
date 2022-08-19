a3511da4c149f839047d5a52ecd4a609ca0c8b2f
Laurent Thiebault <thiebault.laurent@gmail.com>
2022-08-20 00:30:20 +0200

    chore: upgrade to vite 3

--- packages/showcases/vue/.storybook/preview.js
+++ packages/showcases/vue/.storybook/preview.js
@@ -3,42 +3,6 @@ import '@vtmn/icons/dist/vitamix/font/vitamix.css';
 import { withDesign } from 'storybook-addon-designs';
 import backgrounds from '@vtmn/showcase-core/addons/backgrounds.json';
 import viewports from '@vtmn/showcase-core/addons/viewports.json';
-import dedent from 'ts-dedent';
-import { paramCase } from 'param-case';
-
-const templateSourceCode = (
-  templateSource,
-  args,
-  argTypes,
-  replacing = ' v-bind="args"',
-) => {
-  const componentArgs = {};
-  for (const [k, t] of Object.entries(argTypes)) {
-    const val = args[k];
-    if (typeof val !== 'undefined') {
-      componentArgs[k] = val;
-    }
-  }
-
-  const propToSource = (key, val) => {
-    const type = typeof val;
-    switch (type) {
-      case "boolean":
-        return val ? key : "";
-      case "string":
-        return `${key}="${val}"`;
-      default:
-        return `:${key}="${val}"`;
-    }
-  };
-
-  return templateSource.replace(
-    replacing,
-    Object.keys(componentArgs)
-      .map((key) => " " + propToSource(paramCase(key), args[key]))
-      .join(""),
-  );
-};
 
 export const decorators = [() => ({ template: '<div><story/></div>' }), withDesign];
 
