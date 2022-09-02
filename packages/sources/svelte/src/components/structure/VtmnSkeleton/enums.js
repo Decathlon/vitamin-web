e7861f29ce9a55875411af01bb1b4df80a493a5d
Antoine Vendeville <Tlahey@users.noreply.github.com>
2022-09-02 15:54:58 +0200

    fix(@vtmn/svelte): add unit param on `VtmnSkeleton` (#1252)
    * fix(@vtmn/svelte): add unit param on VtmnSkeleton
    
    * fix(@vtmn/svelte): fix some comments
    
    * fix(@vtmn/svelte): code review fix
    
    * fix(@vtmn/svelte): code review fix - 2
    
    * fix(@vtmn/svelte): code review fix - 3
    
    * fix(@vtmn/svelte): code review fix - 4
    
    * fix(@vtmn/svelte): code review fix - 5
    
    Co-authored-by: Gaspard Mathon <gaspard.mathon@hotmail.fr>

--- packages/sources/svelte/src/components/structure/VtmnSkeleton/enums.js
+++ packages/sources/svelte/src/components/structure/VtmnSkeleton/enums.js
@@ -5,3 +5,15 @@ export const VTMN_SKELETON_SHAPE = {
   LINE: 'line',
   AVATAR: 'avatar',
 };
+
+export const VTMN_SKELETON_UNIT = {
+  EM: 'em',
+  REM: 'rem',
+  VW: 'vw',
+  CH: 'ch',
+  PERCENT: '%',
+  PX: 'px',
+};
+
+export const VTMN_SKELETON_DEFAULT_WIDTH = 100;
+export const VTMN_SKELETON_DEFAULT_UNIT = VTMN_SKELETON_UNIT.PERCENT;
