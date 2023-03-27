9e408602718bce763aaecf4fc66cad5ad0632c6a
Térence Hollander <hollanderterence@gmail.com>
2023-03-27 09:52:01 +0200

    fix(VtmnSkeleton): width should be passed with free unit
    unit has non-sense to be managed here, we should
    instead rely on CSS API instead of managing all possible
    unit values inside our components
    if the value is not correct, CSS will manage it its way
    
    BREAKING CHANGE: remove `unit` from `VtmnSkeleton`, it should now be passed in the `width` or `height` directly

--- packages/sources/svelte/src/components/structure/VtmnSkeleton/VtmnSkeleton.svelte
+++ packages/sources/svelte/src/components/structure/VtmnSkeleton/VtmnSkeleton.svelte
@@ -1,24 +1,13 @@
 <script>
   import { cn } from '../../../utils/classnames';
-  import {
-    VTMN_SKELETON_SHAPE,
-    VTMN_SKELETON_UNIT,
-    VTMN_SKELETON_DEFAULT_UNIT,
-    VTMN_SKELETON_DEFAULT_WIDTH,
-  } from './enums';
+  import { VTMN_SKELETON_SHAPE, VTMN_SKELETON_DEFAULT_WIDTH } from './enums';
   import { objectToStyle } from '../../../utils/style';
 
   /**
-   * @type {number} Width applied on the skeleton.
-   * @defaultValue 100
+   * @type {string} Width applied on the skeleton.
+   * @defaultValue 100%
    */
   export let width = VTMN_SKELETON_DEFAULT_WIDTH;
-
-  /**
-   * @type {'%'|'rem'|'em'|'px'|'vw'|'ch'} Unit applied on the width.
-   * @defaultValue %
-   */
-  export let unit = VTMN_SKELETON_DEFAULT_UNIT;
 
   /**
    * @type {'line' | 'avatar' } Variant of the shape.
@@ -39,21 +28,8 @@
     className,
   );
 
-  let computedUnit;
-  let computedWidth;
-
-  $: {
-    if (width < 0 || !Object.values(VTMN_SKELETON_UNIT).includes(unit)) {
-      computedWidth = VTMN_SKELETON_DEFAULT_WIDTH;
-      computedUnit = VTMN_SKELETON_DEFAULT_UNIT;
-    } else {
-      computedWidth = width;
-      computedUnit = unit;
-    }
-  }
-
   $: componentStyle = objectToStyle({
-    '--skeleton-width': `${computedWidth}${computedUnit}`,
+    '--skeleton-width': width,
   });
 </script>
 
