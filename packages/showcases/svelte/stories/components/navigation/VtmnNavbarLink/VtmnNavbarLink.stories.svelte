bc47c6419de0e9adc9b99dfeba76ecd00fb98323
Vendeville Antoine <antoine.vendeville@decathlon.com>
2023-04-27 15:37:53 +0200

    feat(VtmnNavbarLink): clean style

--- packages/showcases/svelte/stories/components/navigation/VtmnNavbarLink/VtmnNavbarLink.stories.svelte
+++ packages/showcases/svelte/stories/components/navigation/VtmnNavbarLink/VtmnNavbarLink.stories.svelte
@@ -50,17 +50,3 @@
     </VtmnNavbarLink>
   </div>
 </Story>
-
-<style>
-  :global(.vtmn-sr-only) {
-    position: absolute;
-    width: 1px;
-    height: 1px;
-    padding: 0;
-    margin: -1px;
-    overflow: hidden;
-    clip: rect(0, 0, 0, 0);
-    white-space: nowrap;
-    border-width: 0;
-  }
-</style>
